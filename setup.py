db833cc911b696da51ad6584a788ffc02eb05958
0xArti <artium8420@gmail.com>
2020-12-25 17:37:42 +0200

    updated setup and manifest, added configurations folder

--- setup.py
+++ setup.py
@@ -1,14 +1,29 @@
+import sys
 import setuptools
+
+
+def get_excludes():
+    excludes = ["*test"]
+    if sys.platform != "win32":
+        excludes.append("service")
+
+
+def get_requirements():
+    requirements = [
+        "PySimpleGUI"
+    ]
+    if sys.platform == "win32":
+        requirements.append("pywin32")
+    
+    return requirements
+
 
 setuptools.setup(
     name="mobility-gui",
     version="0.1",
     author="Art",
     description="Cross-platform service for displaying mobility exercises on screen",
-    packages=setuptools.find_packages(exclude=("*test")),
+    packages=setuptools.find_packages(exclude=get_excludes()),
     python_requires=">=3.6",
-    install_requires=[
-        "pywin32",
-        "PySimpleGUI",
-    ]
+    install_requires=get_requirements()
 )
